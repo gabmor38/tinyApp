e2a16ac67a4c32c4ab3ad2dc828c8a72020898f4
gabmor38 <Gabriela.morenor@gmail.co>
2021-08-28 18:50:19 -0400

    added templateVars

--- express_server.js
+++ express_server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 8080; //default port 8080
 
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get('/',(req, res) => {
+app.get('/',(req, res) => { // if they pass the home page return hello
   res.send('Hello!');
 });
 
@@ -19,6 +19,11 @@ app.get('/urls.json',(req, res) => {
 
 app.get('/hello', (req,res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
+});
+
+app.get('/urls',(req, res) => { // localhost:8080/urls will show the ejs
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);  //urls_index is the filename inside the views file folder, don't need to put the extension.
 });
 
 app.listen(PORT, () => {
