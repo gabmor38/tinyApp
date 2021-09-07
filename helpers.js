0c6143cb9ede9a5f7964b2d16e424b97454e5313
gabmor38 <Gabriela.morenor@gmail.co>
2021-09-06 20:56:52 -0400

    created a helper function and moved getuseremail functions

---
+++ helpers.js
@@ -0,0 +1,10 @@
+const getUserEmail = (email, users) => {
+  for (const id in users) {
+    if (users[id].email === email) {
+      return users[id];
+    }
+  }
+  return null;
+};
+
+module.exports = { getUserEmail };
